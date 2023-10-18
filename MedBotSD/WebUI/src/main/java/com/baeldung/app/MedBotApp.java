f5d1579d1c06c8de9b58fba7fabd41218bc0ac98
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-10-18 19:18:15 +0300

    add

--- MedBotSD/WebUI/src/main/java/com/baeldung/app/MedBotApp.java
+++ MedBotSD/WebUI/src/main/java/com/baeldung/app/MedBotApp.java
@@ -1,9 +1,12 @@
-package com.baeldung;
+package com.baeldung.app;
 
+import com.baeldung.controller.DoctorController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = DoctorController.class)
 public class MedBotApp {
 
     public static void main(String[] args) {
--- MedBotSD/WebUI/src/main/java/com/baeldung/controller/DoctorController.java
+++ MedBotSD/WebUI/src/main/java/com/baeldung/controller/DoctorController.java
@@ -1,2 +1,47 @@
-package com.baeldung.controller;public class DoctorController {
+package com.baeldung.controller;
+
+import com.baeldung.Doctor.Doctor;
+import com.baeldung.Doctor.IDoctorService;
+import com.baeldung.config.AppConfig;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+public class DoctorController {
+
+    private IDoctorService doctorService;
+
+    public DoctorController() {
+        AppConfig appConfig;
+        try {
+            appConfig = new AppConfig();
+            doctorService = appConfig.getDoctorServiceImpl();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/doctors")
+    protected ResponseEntity<Object> doGet(@RequestParam String specialization,
+                                           @RequestParam int limit, @RequestParam int skipped) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/doctors")
+    protected ResponseEntity<Object> doPost(@RequestBody Doctor doctor) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/doctors/{id}")
+    protected ResponseEntity<Object> doGet(@PathVariable int id) {
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @PutMapping("/doctors/{id}")
+    protected ResponseEntity<Object> doPut(@PathVariable int id, @RequestBody Doctor doctor) {
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 }
