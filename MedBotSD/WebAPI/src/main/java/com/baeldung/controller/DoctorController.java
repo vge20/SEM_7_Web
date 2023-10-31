de982219c9b788b3b0c3ecc3aaaf296bb120f1b1
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-11-01 00:42:50 +0300

    add

--- MedBotSD/WebAPI/src/main/java/com/baeldung/controller/DoctorController.java
+++ MedBotSD/WebAPI/src/main/java/com/baeldung/controller/DoctorController.java
@@ -3,15 +3,14 @@ package com.baeldung.controller;
 import com.baeldung.Doctor.IDoctorService;
 import com.baeldung.Doctor.Doctor;
 import com.baeldung.config.AppConfig;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.print.Doc;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class DoctorController {
 
@@ -26,58 +25,47 @@ public class DoctorController {
         }
     }
 
-    private HttpHeaders generateHttpHeaders() {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Access-Control-Allow-Origin", "*");
-        httpHeaders.add("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-        httpHeaders.add("Access-Control-Allow-Headers", "*");
-        return httpHeaders;
-    }
-
     @GetMapping("/api/v1/doctors")
     protected ResponseEntity<Object> doGet(@RequestParam String specialization, @RequestParam int limit,
                                            @RequestParam int skipped) {
         List<Doctor> doctorsList = doctorService.getDoctorsList(specialization, limit, skipped);
-        return new ResponseEntity<>(doctorsList, generateHttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(doctorsList, HttpStatus.OK);
     }
 
     @GetMapping("/api/v1/doctors/{id}")
     protected ResponseEntity<Object> doGet(@PathVariable int id) {
         Doctor doctor = doctorService.getDoctorById(id);
-        return new ResponseEntity<>(doctor, generateHttpHeaders(), HttpStatus.OK);
+        return new ResponseEntity<>(doctor, HttpStatus.OK);
     }
 
     @PostMapping("/api/v1/doctors")
     protected ResponseEntity<Object> doPost(@RequestBody Doctor doctor) {
-        System.out.println("AAA");
         if (doctorService.addDoctor(doctor)) {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.CREATED);
+            return new ResponseEntity<>(HttpStatus.CREATED);
         }
         else {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PutMapping("/api/v1/doctors/{id}")
     protected ResponseEntity<Object> doPut(@RequestBody Doctor doctor, @PathVariable int id) {
-        System.out.println("AAA");
         doctor.setId(id);
         if (doctorService.updateDoctor(doctor)) {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
         else {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/api/v1/doctors/{id}")
     protected ResponseEntity<Object> doDelete(@PathVariable int id) {
-        System.out.println("AAA");
         if (doctorService.deleteDoctor(id)) {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
         else {
-            return new ResponseEntity<>(generateHttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
