690a91d9276c82adb096f08a5bf1243b9245a55f
Gleb Vagapov <glebka.vagapov@mail.ru>
2023-11-02 01:20:59 +0300

    add

--- MedBotSD/WebAPI/src/main/java/com/baeldung/controller/PatientController.java
+++ MedBotSD/WebAPI/src/main/java/com/baeldung/controller/PatientController.java
@@ -3,12 +3,11 @@ package com.baeldung.controller;
 import com.baeldung.User.IUserService;
 import com.baeldung.User.User;
 import com.baeldung.config.AppConfig;
+import com.baeldung.dto.PatientDTO;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -37,6 +36,30 @@ public class PatientController {
         }
         else {
             return new ResponseEntity<>(usersList, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/api/v1/patients/{login}")
+    protected ResponseEntity<Object> doGet(@PathVariable String login) {
+        User user = userService.getUserByLogin(login);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        else {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/api/v1/patients")
+    protected ResponseEntity<Object> doPost(@RequestBody PatientDTO patientDTO) {
+        User user = new User(patientDTO.getLogin(), patientDTO.getPassword(), 0,
+                patientDTO.getFirstName(), patientDTO.getLastName(),
+                patientDTO.getGender(), patientDTO.getBirthDate());
+        if (userService.addUser(user)) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
